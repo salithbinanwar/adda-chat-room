b6d4c4242cfa523d8a059ad8fb0fab4b43e2ce7b
sazzad-anwarm <sazzadzihan@gmail.com>
2021-12-21 01:10:54 +0600

    name message added

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const socket = io('https://adda-chat.herokuapp.com/');
 // const socket = io('http://localhost:8080');
 
-socket.on('message', text => {
+socket.on('message', ({ message, name }) => {
 
     let gotName = 'hello'
 
@@ -12,12 +12,12 @@ socket.on('message', text => {
     const el = `
     <li class="flex my-2 w-full">
         <div class="h-11 w-11 rounded-full ring-2 flex justify-center items-center ring-indigo-500/50 mr-2 uppercase">
-            ${(localStorage.getItem("lastname")).substring(0, 1)}
+            ${name.substring(0, 1)}
         </div>
         
         <div class="bg-indigo-500/50 w-9/12 px-4 py-2 rounded-xl">
-        <h6  class='text-sm font-mono'>${localStorage.getItem("lastname")}</h6>
-            <p class="text-sm text-left">${text}</p>
+        <h6  class='text-sm font-mono'>${name}</h6>
+            <p class="text-sm text-left">${message}</p>
             <p class="text-xs pt-.5 text-right">${new Date().toLocaleTimeString('en-US', { day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit' })}</p>
         </div>
     </li>`
@@ -46,7 +46,7 @@ document.querySelector("button").addEventListener('click', e => {
     const text = document.querySelector('textarea').value;
     console.log(e.key)
     if (e.key === 'Enter' && text !== '') {
-        socket.emit('message', text)
+        socket.emit('message', { text, name: localStorage.getItem("lastname") })
         document.querySelector('textarea').value = ''
     } else {
         socket.emit('message', text)
